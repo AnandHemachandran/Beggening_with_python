0bb51ee16a06059055c774e99311328e2bb9ec57
Anand H <41850468+AnandHemachandran@users.noreply.github.com>
2018-09-10 11:35:50 +0530

    Update Concatenation.py

--- Concatenation.py
+++ Concatenation.py
@@ -1,5 +1,6 @@
 string=list()
-a=int(input("Enter the no of strings you want to concatenate : "))
+a=int(input("Enter the no of total strings you want: "))
+z=int(input("How much string do you want to concatenate each at a time: "))
 for x in range(a):
 	n=input("Enter a string : ")
 	string.append(str(n))
@@ -7,7 +8,7 @@ print("Arrray",string)
 b=""
 for y in range(a):
 	b=b+string[y]
-	if((y+1)%3==0):
+	if((y+1)%z==0):
 		print("Original word : ",b)
 		c=b[::-1]
 		print("Reversed word : ",c)	
